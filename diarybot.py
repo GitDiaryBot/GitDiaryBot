32b3651309f23e584d58e6abf9c9629424aab7b1
Peter Demin <peter.demin@moveworks.ai>
2020-01-10 23:15:00 -0500

    add records

--- diarybot.py
+++ diarybot.py
@@ -1,28 +1,63 @@
+#!/usr/bin/env python3
+
 import os
 import logging
+import datetime
 
 from telegram.ext import MessageHandler
 from telegram.ext import Updater
 from telegram.ext import Filters
+import git
+import tzlocal
 
 
-TG_TOKEN = os.environ['DIARY_TOKEN']
-TG_API_URL = "https://telegg.ru/orig/bot"
+DIARY_TOKEN = os.environ['DIARY_TOKEN']
+DIARY_DIR = os.environ['DIARY_DIR']
+DIARY_FILE = os.environ.get('DIARY_FILE', 'README.md')
+DIARY_FILE_PATH = os.path.join(DIARY_DIR, DIARY_FILE)
+RECORD_PTRN = """
+{time}
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+{text}
+"""
 
 
 def main():
-    bot = Updater(TG_TOKEN, TG_API_URL, use_context=True)
-    bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
-    bot.start_polling()  # проверяет о наличии сообщений с платформы Telegram
-    bot.idle()  # бот будет работать пока его не остановят
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+    bot = Updater(DIARY_TOKEN, use_context=True)
+    bot.dispatcher.add_handler(MessageHandler(
+        filters=Filters.text,
+        callback=handler,
+    ))
+    bot.start_polling()
+    bot.idle()
 
 
-def parrot(bot, update):
-    print(bot.message.text)  # печатаем на экран сообщение пользователя
-    bot.message.reply_text(bot.message.text)  # отправляем обратно текс который пользователь послал
+def handler(update, context):
+    del context  # TODO: choose settings for user
+    add_record(update.message.text)
+    update.message.reply_text("Saved")
+
+
+def add_record(text: str):
+    now = datetime.datetime.now(tzlocal.get_localzone())
+    repo = git.Repo(DIARY_DIR)
+    repo.remotes[0].pull()
+    write_record(now, text)
+    repo.index.add(DIARY_FILE)
+    repo.index.commit("add record from GitDiaryBot")
+    repo.remotes[0].push()
+
+
+def write_record(time: datetime.datetime, text: str):
+    with open(DIARY_FILE_PATH, "at") as fp:
+        fp.write(RECORD_PTRN.format(
+            time=time.strftime('%Y/%m/%d %H:%M:%S %Z %z'),
+            text=text,
+        ))
 
 
 if __name__ == '__main__':
